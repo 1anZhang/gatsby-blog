e107a58db471adc44c9fb78c57f83c7f7ce2792a
ZhangYxY <512224856@qq.com>
2019-11-06 20:13:42 +0800

    :sparkles: drag bezier anchor

--- src/components/Bezier/index.jsx
+++ src/components/Bezier/index.jsx
@@ -23,7 +23,8 @@ const Bezier = ({
   height = 200,
   handleColor = '#ff3614',
   handlePointColor = '#12fc14',
-  curveColor = '#049'
+  curveColor = '#049',
+  onAnchorChange
 }) => {
   const cWidth = width * 2;
   const cHeight = height * 2;
@@ -37,8 +38,8 @@ const Bezier = ({
     arr.push({ x: i, y: easing(i) });
   }
   const canvas = useRef(null);
-  let handleWidth = 4;
-  let handleRadius = 6;
+  let handleWidth1 = 4, handleWidth2 = 4;
+  let handleRadius1 = 6,handleRadius2 = 6;
   let ctx;
 
   const draw = () => {
@@ -72,19 +73,21 @@ const Bezier = ({
 
     // draw Handle
     ctx.strokeStyle = handleColor;
-    ctx.lineWidth = handleWidth;
+    ctx.lineWidth = handleWidth1;
     ctx.beginPath();
     ctx.moveTo(0, cHeight);
     ctx.lineTo(x1 * cWidth, cHeight - y1 * cHeight);
+    ctx.stroke();
+    ctx.lineWidth = handleWidth2;
     ctx.moveTo(cWidth, 0);
     ctx.lineTo(x2 * cWidth, cHeight - y2 * cHeight);
     ctx.stroke();
     ctx.fillStyle = handlePointColor;
     ctx.beginPath();
-    ctx.arc(x1 * cWidth, cHeight - y1 * cHeight, handleRadius, 0, 2 * Math.PI);
+    ctx.arc(x1 * cWidth, cHeight - y1 * cHeight, handleRadius1, 0, 2 * Math.PI);
     ctx.fill();
     ctx.beginPath();
-    ctx.arc(x2 * cWidth, cHeight - y2 * cHeight, handleRadius, 0, 2 * Math.PI);
+    ctx.arc(x2 * cWidth, cHeight - y2 * cHeight, handleRadius2, 0, 2 * Math.PI);
     ctx.fill();
 
 
@@ -107,16 +110,23 @@ const Bezier = ({
     };
   };
 
-  const isOnHandle = (pos) => {
-    const pixel = ctx.getImageData(pos.x, pos.y, 1, 1).data;
-    const color = new Color({ r: pixel[0], g: pixel[1], b: pixel[2] });
-    return color.toHexString() === handlePointColor
+  const handleFlag = (pos) => {
+    let flag;
+    let dis1 = (pos.x - x1 * cWidth) * (pos.x - x1 * cWidth) +  (pos.y - cHeight + y1 * cHeight) * (pos.y - cHeight + y1 * cHeight);
+    let dis2 = (pos.x - x2 * cWidth) * (pos.x - x2 * cWidth) +  (pos.y - cHeight + y2 * cHeight) * (pos.y - cHeight + y2 * cHeight);
+    if (dis1 < 40) {
+      flag = 1;
+    } 
+    if (dis2 < 40) {
+      flag = 2;
+    }
+    return flag;
   }
 
   const handleMouseDown = e => {
     const mousePos = getMousePos(e);
-    const isHandle = isOnHandle(mousePos);
-    if (!isHandle) return;
+    const handleIndex = handleFlag(mousePos);
+    if (!handleIndex) return;
     drag = true;
   }
 
@@ -126,20 +136,35 @@ const Bezier = ({
 
   const handleMouseMove = e => {
     const mousePos = getMousePos(e);
-    const isHandle = isOnHandle(mousePos);
+    const handleIndex = handleFlag(mousePos);
     if (drag) {
-      console.log(mousePos);
+      let mx = mousePos.x / cWidth;
+      let my = mousePos.y / cHeight;
+      console.log('xxx', mx, 1- my);
+      if (handleIndex === 1) {
+        x1 = mx;
+        y1 = 1 -my;
+      } else if(handleIndex === 2) {
+        x2 = mx;
+        y2 = 1- my;
+      }
+      // onAnchorChange(mx, 1- my, handleIndex);
     }
-    if (isHandle) {
+    if (handleIndex) {
       hover = true;
-      handleWidth = 10;
-      handleRadius = 12;
+      if (handleIndex === 1) {
+        handleWidth1 = 10;
+        handleRadius1 = 12;
+      } else {
+        handleWidth2 = 10;
+        handleRadius2 = 12;
+      }
       draw();
     } else {
       if (hover) {
         hover = false;
-        handleWidth = 4;
-        handleRadius = 6;
+        handleWidth1 = handleWidth2 = 4;
+        handleRadius1 = handleRadius2 = 6;
         draw();
       }
     }
@@ -157,6 +182,7 @@ const Bezier = ({
         style={{ width: width, height: height }}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       ></canvas>
       <Bottom>{`Params: [${x1}, ${y1}, ${x2}, ${y2}]`}</Bottom>
     </BezierWrapper>
--- src/pages/bezier.js
+++ src/pages/bezier.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Bezier, Seo, Layout } from '../components';
 
-const TreasurePage = () => (
-  <Layout>
-    <Seo title="bezier-curve" />
+const TreasurePage = () => {
+  const [o1, setO1] = useState([{ x: 0.3, y: 0.1},{ x: 0.1, y: 0.8 }]);
+  const [o2, setO2] = useState([{ x: 0.3, y: 0.4},{ x: 0.1, y: 0.8 }]);
+  const handleChange1 = (x, y, i) => {
+    let t = [...o1];
+    t[i-1] = {x, y};
+    setO1(t);
+  };
+  const handleChange2 = (x, y, i) => {
+    let t = [...o2];
+    t[i-1] = {x, y};
+    setO2(t);
+  };
 
-    <Bezier x1={0.3} y1={0.1} x2={0.1} y2={0.8}/>
-    <Bezier x1={0.3} y1={0.4} x2={0.1} y2={0.8}/>
-  </Layout>
-);
+  return (
+    <Layout>
+      <Seo title="bezier-curve" />
+
+      <Bezier
+        x1={o1[0].x}
+        y1={o1[0].y}
+        x2={o1[1].x}
+        y2={o1[1].y}
+        onAnchorChange={handleChange1}
+      />
+      <Bezier
+        x1={o2[0].x}
+        y1={o2[0].y}
+        x2={o2[1].x}
+        y2={o2[1].y}
+        onAnchorChange={handleChange2}
+      />
+    </Layout>
+  );
+};
 
 export default TreasurePage;
